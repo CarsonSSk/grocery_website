c98574f8b913968021c8f68ec54dbf6f93e7ca60
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2021-02-27 12:32:32 -0500

    Add JS functionality to product pages

---
+++ scripts/moreinfo.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".more-info").addEventListener("click", showInfo);
+
+function showInfo() {
+    document.querySelector("div.border").classList.toggle("hidden");
+}
---
+++ scripts/retainquantity.js
@@ -0,0 +1,18 @@
+//Saves value user enters for quantity on each product page, so it remains displayed after refresh
+
+document.getElementById("item").value = getSavedValue("item"); //Find value entered by user when a refresh happens
+                    
+//Save the input to local storage with user id and value
+function saveValue(passedValue){
+    var id = passedValue.id;  //Get user ID
+    var val = passedValue.value; //Get user input
+    localStorage.setItem(id, val); //Update local storage when value changes
+}
+
+//If a value is stored locally, display it on refresh
+function getSavedValue(value){
+    if (localStorage.getItem(value)) {
+        return localStorage.getItem(value);
+    }
+    
+}
