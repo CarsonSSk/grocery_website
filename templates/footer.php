451335e6b69debcb502b194479ce3bff436ce7e8
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2021-04-03 13:08:15 -0400

    Dynamic insertion of headers and footers

---
+++ templates/footer.php
@@ -0,0 +1,23 @@
+<footer>
+      <div class="row page-width">
+
+        <div class="col-lg-3 col-md-12">
+          <p class="footer-name"><a href="/Grocery/grocery_website/index.php">TastyGrocery.com</a></p>
+        </div>
+
+        <div class="col-lg-6 col-md-12">
+          <div class="icon-bar-foot">
+            <a href="https://www.facebook.com" target="_blank" class="facebook"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.twitter.com" target="_blank" class="twitter"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.pinterest.com" target="_blank" class="pinterest"><i class="fab fa-pinterest"></i></a>
+            <a href="https://www.tumblr.com" target="_blank" class="tumblr"><i class="fab fa-tumblr"></i></a>
+            <a href="mailto:?" class="email"><i class="far fa-envelope"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-12">
+          <p class="contact"><a href="mailto:grocery@mailinator.com">Contact us</a></p>
+        </div>
+
+      </div>
+</footer>
---
+++ templates/navbar.php
@@ -0,0 +1,10 @@
+<div class="header-userpage">
+      <div class="row page-width">
+        <div class="col-6">
+          <a href="/Grocery/grocery_website/userlist.html"><i class="fas fa-door-open"></i>Backstore</a>
+        </div>
+        <div class="col-6 right-text">
+          <a href="/Grocery/grocery_website/login.php"><i class="fas fa-user-alt"></i>My Account</a>
+        </div>
+      </div>
+    </div>
